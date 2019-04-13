90ebbf8427841d609883a83ec43be66acd7df39f
VeronicaFrota <veronicafrota2011@hotmail.com>
2019-04-13 13:27:18 -0300

    criando componente de escolha de forma de pagamento usando contralValueAccesso - Opções de escolha com radio button

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
 
 @Component({
-  selector: 'mt-order',
-  templateUrl: './order.component.html'
+	selector: 'mt-order',
+	templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+	// Para representar as opções que serão utilizadas no radio de escolha de forma de pagamento
+	paymentOptions: RadioOption[] = [
+		{ label: 'Dinheiro', value: 'MON' },
+		{ label: 'Cartão de Débito', value: 'DEB' },
+		{ label: 'Cartão Refeição', value: 'REF' }
+	]
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
